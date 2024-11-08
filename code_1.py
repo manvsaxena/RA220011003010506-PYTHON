386a35c3ebbe564367727fd47982cb9e3bfa1e0d
Manav Saxena <126712194+manvsaxena@users.noreply.github.com>
2024-11-08 19:40:30 +0530

    Add files via upload

---
+++ code_1.py
@@ -0,0 +1,18 @@
+#Personal Message
+
+print("Hello Eric, would you like to learn some Python today?")
+
+#Uppercase and Lowercase in Python
+
+name = "manav"
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+#printing famous quote
+author_firstname = "albert "
+author_secondname = "einstein"
+quote = "a person who never made a mistake never tried anything new"
+
+print(author_firstname.title()  +  author_secondname.title()  + " who said,"+'"'+ quote+'".')
---
+++ code_2.py
@@ -0,0 +1,30 @@
+#addition
+
+print(5+9)
+
+#multiplication 
+
+print(5*9)
+
+#substraction
+
+print(9-5)
+
+#division
+
+print(10/5)
+
+#modulus
+
+print(10%5)
+
+#using variables
+
+a = 10
+b = 20 
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
