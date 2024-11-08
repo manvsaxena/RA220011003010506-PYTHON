808d0e7fe427d7ba071421cdab7197456b856295
Manav Saxena <126712194+manvsaxena@users.noreply.github.com>
2024-11-08 19:55:43 +0530

    Add files via upload

---
+++ code_3.py
@@ -0,0 +1,25 @@
+#creating a list called names
+
+names = ["manav","kshitij","sanchari","keshav","vedant","shorya"]
+
+print(names[0].title() + " is a good boy")
+print(names[1].title() + " is a bad boy")
+print(names[3].title() + " & " + names[5].title()+ " are brothers")
+
+#car names in a list
+
+a = "lamborghini"
+b = "ferrari"
+c = "aston martin"
+d = "alfa romeo"
+
+cars = [a,b,c,d]
+
+print(cars[1].title() + " comes first in F1 racing")
+print(cars[0].title() + " comes second in F1 racing")
+print(cars[2].title() + " comes third in F1 racing")
+print(cars[3].title() + " crashed")
+
+
+
+
