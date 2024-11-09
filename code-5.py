f6787360bb76d6d90928cab18b951754b4f436da
Manav Saxena <126712194+manvsaxena@users.noreply.github.com>
2024-11-09 19:18:03 +0530

    Add files via upload

---
+++ code-5.py
@@ -0,0 +1,20 @@
+#unsorted list
+unsorted_list = ["sanchari","manav","kshtitj","shorya","keshav"]
+
+print("Unsorted list(Original): ")
+print(unsorted_list)
+
+#sorted list - temporary
+print(sorted(unsorted_list))
+
+#sorted list - permanent
+unsorted_list.sort()
+print(unsorted_list)
+
+#revereing the sorted list
+unsorted_list.reverse()
+print(unsorted_list)
+
+#length of the list
+
+print(len(unsorted_list))
