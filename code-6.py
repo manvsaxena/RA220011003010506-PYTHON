8d365da2393890ae9dd0148bd0f289e781d3a760
Manav Saxena <126712194+manvsaxena@users.noreply.github.com>
2024-11-09 19:28:15 +0530

    Add files via upload

---
+++ code-6.py
@@ -0,0 +1,16 @@
+#for loop for list pizza
+
+fav_pizza = ["margerita" , "farmhouse" , "veggie paradise"]
+
+for i in fav_pizza:
+    print("I like " + i.title() + " Pizza")
+    print("I really love pizza! ")
+
+#for loop for list animals
+
+animals = ["dog","lion","elephant","horse","cat"]
+
+for i in animals:
+    print("A " + i.title() + " would make a great pet.")
+    print("Any of these animals would make a great pet!")
+    
