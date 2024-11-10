493ae4e41e0e96e5691cb3d65bb547ae460e315e
Manav Saxena <126712194+manvsaxena@users.noreply.github.com>
2024-11-10 18:46:28 +0530

    Add files via upload

---
+++ code-8.py
@@ -0,0 +1,21 @@
+#first three items in the list
+
+list_one = ["apple","banana","chips","coke","diary milk"]
+
+for i in list_one[:3]:
+    print(i)
+print("first 3 item(above): ")
+
+#middle of the list
+
+for i in list_one[1:4]:
+    print(i)
+
+print("middle 3 item(above): ")
+
+#last of the list
+
+print("last three items: ")
+
+for i in list_one[2:]:
+    print(i)
